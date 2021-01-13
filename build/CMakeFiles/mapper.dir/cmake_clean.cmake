1ae36882c6d454e43c175f6610c8008e17a9e1de
lsscecilia <lsscecilia8888@gmail.com>
2021-01-13 11:02:04 +0800

    initial commit

---
+++ build/CMakeFiles/mapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/mapper"
+  "../bin/mapper.pdb"
+  "CMakeFiles/mapper.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
